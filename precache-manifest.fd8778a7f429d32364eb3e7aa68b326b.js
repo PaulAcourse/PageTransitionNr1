a5e29593193e77a89a660d2a78c2b0aa75f906a0
PaulAcourse <paul.cichon@acourse.de>
2021-12-04 15:44:09 +0100

    gh-pages

---
+++ precache-manifest.fd8778a7f429d32364eb3e7aa68b326b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cfc359e7223d4010169",
+    "url": "/PageTransitionNr1/css/app.51bbaeee.css"
+  },
+  {
+    "revision": "ee93ee4590a8bc611c7c70e565cc3839",
+    "url": "/PageTransitionNr1/index.html"
+  },
+  {
+    "revision": "4cfc359e7223d4010169",
+    "url": "/PageTransitionNr1/js/app.193f0aa4.js"
+  },
+  {
+    "revision": "1877c508513e5cab64d2",
+    "url": "/PageTransitionNr1/js/chunk-vendors.679b0b22.js"
+  },
+  {
+    "revision": "de5fafa9aeebb426b6ca3a8794138cf7",
+    "url": "/PageTransitionNr1/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/PageTransitionNr1/robots.txt"
+  }
+]);
